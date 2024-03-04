b48c912f5e36872cf307a99bebf2f3f6804f71a9
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-03-04 09:02:53 +0000

    Revert to mysql:5.7 image

--- .devcontainer/init/init.sql
+++ .devcontainer/init/init.sql
@@ -9,7 +9,7 @@ CREATE USER 'orchestrator'@'%' IDENTIFIED BY 'root';
 CREATE USER 'workflow'@'%' IDENTIFIED BY 'root';
 
 GRANT ALL PRIVILEGES ON orchestrator.* TO 'orchestrator'@'%';
-GRANT XA_RECOVER_ADMIN ON *.* TO 'orchestrator'@'%';
+-- GRANT XA_RECOVER_ADMIN ON *.* TO 'orchestrator'@'%';
 
 GRANT ALL PRIVILEGES ON workflow.* TO 'workflow'@'%';
-GRANT XA_RECOVER_ADMIN ON *.* TO 'workflow'@'%';
+-- GRANT XA_RECOVER_ADMIN ON *.* TO 'workflow'@'%';

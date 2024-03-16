f16877442beefc168d74706cfc65d825e595045e
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-03-16 09:53:44 +0000

    Make oidc-add interactive for account loading

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 echo alias export-oidc-env=\'eval \$\(more /oidc-agent-config/oidc-agent.env\)\' >> /home/node/.bashrc
 echo alias orchent=\'export-oidc-env \&\& docker run --rm -e ORCHENT_URL=\$ORCHENT_URL -e OIDC_SOCK=\$OIDC_SOCK -e ORCHENT_AGENT_ACCOUNT=\$ORCHENT_AGENT_ACCOUNT indigodatacloud/orchent\' >> /home/node/.bashrc
-echo alias oidc-add=\'docker exec orchestrator-devc-oidc-agent oidc-add\' >> /home/node/.bashrc
+echo alias oidc-add=\'docker exec -it orchestrator-devc-oidc-agent oidc-add\' >> /home/node/.bashrc
 echo alias oidc-gen=\'docker exec -it orchestrator-devc-oidc-agent oidc-gen\' >> /home/node/.bashrc
 echo alias oidc-token=\'docker exec orchestrator-devc-oidc-agent oidc-token\' >> /home/node/.bashrc

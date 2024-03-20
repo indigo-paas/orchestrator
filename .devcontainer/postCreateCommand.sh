05488c9103e99242c11103816b5b19d602f788b4
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-03-20 16:26:13 +0100

    Add OIDC_AGENT_CONTAINER_NAME env

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
-echo alias oidc-add=\'docker exec -it orchestrator-devc-oidc-agent oidc-add\' >> /home/node/.bashrc
-echo alias oidc-gen=\'docker exec -it orchestrator-devc-oidc-agent oidc-gen\' >> /home/node/.bashrc
-echo alias oidc-token=\'docker exec orchestrator-devc-oidc-agent oidc-token\' >> /home/node/.bashrc
+echo alias oidc-add=\'docker exec -it \${OIDC_AGENT_CONTAINER_NAME} oidc-add\' >> /home/node/.bashrc
+echo alias oidc-gen=\'docker exec -it \${OIDC_AGENT_CONTAINER_NAME} oidc-gen\' >> /home/node/.bashrc
+echo alias oidc-token=\'docker exec \${OIDC_AGENT_CONTAINER_NAME} oidc-token\' >> /home/node/.bashrc
 # echo alias export-oidc-env=\'eval \$\(more /oidc-agent-config/oidc-agent.env \| sed \'s\/tmp\/oidc-agent-config\/\'\)\' >> /home/node/.bashrc
 echo alias export-oidc-token=\'export ORCHENT_TOKEN=\$\(oidc-token \$ORCHENT_AGENT_ACCOUNT \)\' >> /home/node/.bashrc
 # echo alias orchent=\'export-oidc-env \&\& orchent\' >> /home/node/.bashrc

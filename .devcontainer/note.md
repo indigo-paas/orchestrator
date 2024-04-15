b6af421fd3aa724fa56b6e9057d89a2eb908aa22
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-04-15 11:33:40 +0200

    Do not track temporaneous files

--- .devcontainer/note.md
+++
@@ -1,3 +0,0 @@
-# FLOWS
-Provare a passare il BPMN con taskservice a flowable ui per la validazione.
-
--- .devcontainer/oidc-account-auto-load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IFS=$'\n'; usable=($(oidc-add -l)); unset IFS
-IFS=$'\n'; loaded=($(oidc-add -a)); unset IFS
-missing=(`echo ${usable[@]} ${loaded[@]} | tr ' ' '\n' | sort | uniq -u `)
-for account in "${missing[@]}"; do
-    oidc-add $account
-done
